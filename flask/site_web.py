20e354b50332249c317d3ab4bbc1198f340ea5b5
Lucas <mainguy701@gmail.com>
2023-03-01 16:01:29 +0100

    fusion avec un code qui marche

--- flask/site_web.py
+++ flask/site_web.py
@@ -1,57 +1,104 @@
-from flask import Flask, render_template, request
-import sqlite3 as sql
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+import os
+from flask_migrate import Migrate
 
-app = Flask(__name__)  # création d'alias    
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SECRET_KEY'] = 'azerty'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+Migrate(app, db)
+print("migration")
 
-@app.route("/")        # premiére vue, quand l'url sera " / " la page afficher sera index.html
-def index():           # faire attention de ne pas avoir de doublon dans le nom de la fonction 
-    return render_template("index.html")   # retourne ver la page html 
-
-@app.route("/graphique")
-def graphique():
-    return  render_template("graphique.html")
-
-@app.route("/connecter")
-def connecter():
-    return  render_template("connecter.html")
-
-@app.route("/a_propos")
-def a_propos():
-    return  render_template("a_propos.html")
-
-@app.route("/inscription")    # pour la page de connection je vérifie que j'arrive a me connecter a la BDD
-def inscription():            # et je vérifie que la table existe
-    conn = sql.connect('database.db')  # connection a la base database.db
-    print (conn)
-    print("Connexion faite !!!")
-    conn.execute('CREATE TABLE IF NOT EXISTS users(nom VARCHAR(255), prenom VARCHAR(255), email VARCHAR(255), mdp VARCHAR(255), date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP);')
-    return  render_template("inscription.html")
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 
-
-@app.route('/addrec',methods = ['POST', 'GET'])
-def addrec():
-    if request.method == 'POST':
-        try:
-            nom = request.form['nom']
-            prenom = request.form['prenom']
-            email = request.form['email']
-            mdp = request.form['mdp']
-
-            with sql.connect("database.db") as con:
-                cur = con.cursor()
-                cur.execute("INSERT INTO users (nom,prenom,email,mdp) VALUES (?,?,?,?)",(nom,prenom,email,mdp) )
-            
-            con.commit()
-            msg = "Enregistrement correctement envoyer"
-        except:
-            con.rollback()
-            msg = "Erreur de l'enregistrement"
-        finally:
-            return render_template("graphique.html",msg = msg)
-            con.close()
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('connecter.html', form=form)
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('inscription.html', form=form)
 
 
 if __name__ == "__main__":
