99321482e95d689d0db1e32b966e511447d87d76
Lucas <mainguy701@gmail.com>
2023-03-30 09:07:27 +0200

    version démo

--- flask/test.py
+++ flask/test.py
@@ -32,16 +32,29 @@
 
 # print(check_password_hash(hmdp,"a"))
 
-from jinja2 import Template
 
-template="""
-        <h1>1</h1>
-        <h1>1</h1>
-        <h1>1</h1>
-        <h1>1</h1>
-        <h1>1</h1>
-        <h1>1</h1>
-        <h1>1</h1>
-"""
+import smtplib, ssl
 
-print(template.render)
+# on rentre les renseignements pris sur le site du fournisseur
+smtp_address = 'smtp.gmail.com'
+smtp_port = 465
+
+# on rentre les informations sur notre adresse e-mail
+email_address = 'mainguy701@gmail.com'
+email_password = 'mwrnqzkuwgalfdvf'
+
+# on rentre les informations sur le destinataire
+email_receiver = 'm.querquand@lacroixrouge-brest.fr'
+
+# on crée la connexion
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_address, smtp_port, context=context) as server:
+        # connexion au compte
+        server.login(email_address, email_password)
+        # envoi du mail
+        server.sendmail(email_address, email_receiver, 'Message automatique !!!!!')
+
+
+
+
+#mwrn qzku wgal fdvf
